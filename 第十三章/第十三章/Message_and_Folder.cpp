b22c975f6fc13276f8b83389b260d5b9889b4feb
npjtwy <nwpuwy.qq.com>
2016-10-14 23:13:31 +0800

    完善了 Folder 类

--- 第十三章/第十三章/Message_and_Folder.cpp
+++ 第十三章/第十三章/Message_and_Folder.cpp
@@ -1,5 +1,7 @@
 #include "Message_and_Folder.h"
 
+//Message 实现
+//从给定Folder中添加或者删除 Message
 void Message::save(Folder &f)
 {
 	folders.insert(&f);		//将包含本Message的Folder添加进folder
@@ -9,6 +11,16 @@ void Message::remove(Folder &f)
 {
 	folders.erase(&f);
 	f.remMsg(this);
+}
+
+//实现向folders中添加或者删除元素
+void Message::remFld(Folder *f)
+{
+	folders.erase(f);
+}
+void Message::addFld(Folder *f)
+{
+	folders.insert(f);
 }
 
 //拷贝控制成员 用来管理指向 本 message的指针
@@ -75,9 +87,44 @@ void swap(Message &lm, Message &rm)
 	}
 }
 
-
-
-
+//Folder 实现
+inline
+void swap(Folder &lf, Folder &rf)
+{
+	;
+}
+Folder::Folder(const Folder &f)
+{
+	messages = f.messages;
+	add_to_Message(f);		//向 f 所指向的 Message 的 folders 中添加本 Folder
+}
+Folder::~Folder()
+{
+	remove_from_Message();		//将本 Folder 从所指的 Message 的 folders 中删除
+}
+Folder& Folder::operator=(const Folder &f)
+{
+	remove_from_Message();	//先把自己从所指的 Message 的 folders 中删除 在添加到 f 所指的 Message
+	add_to_Message(f);
+	messages = f.messages;
+	return *this;
+}
+//将自身从所指的 Message 中的 folders 中删除或者添加进去
+void Folder::add_to_Message(const Folder &f)
+{
+	for (auto m : f.messages)
+	{
+		m->addFld(this);
+	}
+}
+void Folder::remove_from_Message()
+{
+	for (auto m : messages)
+	{
+		m->remFld(this);
+	}
+}
+//从 messages 中移除或添加指定的 Message
 void Folder::remMsg(Message *m)
 {
 	messages.erase(m);
--- 第十三章/第十三章/Message_and_Folder.h
+++ 第十三章/第十三章/Message_and_Folder.h
@@ -4,11 +4,11 @@
 #include <memory>
 #include <set>
 class Folder;
-
 class Message
 {
-	friend class Folder;
+	friend void swap(Folder &lf, Folder &rf);
 	friend void swap(Message &lm, Message &rm);
+	friend class Folder;
 public:
 	//隐式地将folders初始化为空集 由于参数具有默认值 因此也是默认构造函数
 	explicit Message(const std::string &s = "") : contents(s) { }  
@@ -19,6 +19,9 @@ public:
 	//从给定Folder中添加或者删除 Message
 	void save(Folder&);
 	void remove(Folder&);
+	//实现向folders中添加或者删除元素
+	void remFld(Folder *f);
+	void addFld(Folder *f);
 	
 private:
 	//拷贝控制函数要使用的工具函数
@@ -34,11 +37,22 @@ private:
 
 class Folder
 {
+	friend class Message;
+	friend void swap(Message &lm, Message &rm);
+	friend void swap(Folder &lf, Folder &rf);
 public:
+	Folder() = default;
+	Folder(const Folder&);
+	~Folder();
+	Folder& operator=(const Folder &f);
 	//实现添加或者删除 Message
 	//实现添加或者删除 Message
 	void remMsg(Message *m);
 	void addMsg(Message *m);
 private:
 	std::set<Message*>		messages;		//保存所指向的 Message 指针
+
+	void add_to_Message(const Folder &f);
+	void remove_from_Message();
 };
+void swap(Folder &lf, Folder &rf);
