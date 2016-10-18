8d9fb3b84cd3169d866ab84e86edb6bfe41edb1e
npjtwy <nwpuwy.qq.com>
2016-10-18 09:47:20 +0800

    添加测试用例

--- 第十三章/第十三章/StrVec_test.cpp
+++ 第十三章/第十三章/StrVec_test.cpp
@@ -4,8 +4,15 @@
 
 int main()
 {
-
-
+	{
+		StrVec sv{ "hell", "string" };
+		sv.push_back("niam");
+		sv.resize(5, "aa");
+		for (auto i : sv)
+		{
+			std::cout << i << std::endl;
+		}
+	}
 
 	system("pause");
 	return 0;
